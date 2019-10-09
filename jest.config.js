b98a6f63a4c4fad5c2837661bb5b257bc2855407
Stephane P. Pericat <contact@pericat.net>
2019-10-08 20:56:56 -0500

    basic tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/**/*.{js,jsx}",
-    "!src/index.js",
+    "**/src/js/*.js",
+    "!**/src/index.js",
     "!**/node_modules/**"
   ],
   coverageDirectory: "coverage",
+  setupFiles: ["<rootDir>/jest.setup.js"],
   transform: {
     ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
