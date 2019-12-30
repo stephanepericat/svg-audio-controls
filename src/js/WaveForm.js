bd2abad328875734e47f11c1f48539f854daf996
Stephane P. Pericat <contact@pericat.net>
2019-12-29 18:49:38 -0600

    render waveform

--- src/js/WaveForm.js
+++ src/js/WaveForm.js
@@ -25,6 +25,26 @@ export default class WaveForm extends AudioControl {
   /**
    * Private methods
    */
+
+  _drawFrame(frame, color, width = 1) {
+    this._instance.line(...frame).stroke({ width, color });
+  }
+
+  _drawMedianLine() {
+    const frame = [0, this.median, this.width, this.median];
+    this._drawFrame(frame, this.shadowColor, 1);
+  }
+
+  _drawShadow() {
+    this._instance
+      .rect(this.width, this.median)
+      .move(0, this.median)
+      .attr({
+        fill: this.shadowColor,
+        "fill-opacity": this.shadowOpacity
+      });
+  }
+
   _getAverages(buffer) {
     const d = this._getData(buffer);
     const chunks = this._splitBy(this.size, d);
@@ -38,16 +58,25 @@ export default class WaveForm extends AudioControl {
     return buffer.getChannelData(channel);
   }
 
-  _render() {
-    if (!this.audioData) {
-      throw new Error("No AudioBuffer provided");
-    }
+  _getFrame(frame, xPosition) {
+    const [min, max] = frame;
+    const y1 = this.median - Math.ceil(min * this.median);
+    const y2 = this.median - Math.floor(max * this.median);
+    return [xPosition, y1, xPosition, y2];
+  }
 
-    if (!this.audioLength || !this.channels) {
-      throw new Error("AudioBuffer is empty");
-    }
+  _getFrames(waveFormData) {
+    return waveFormData.map(this._getFrame.bind(this));
+  }
 
-    console.log("WAVEFORM DATA >>>", this.waveFormData);
+  _render(data) {
+    const waveFormData = this._getAverages(data);
+    const frames = this._getFrames(waveFormData);
+    frames.forEach(f => this._drawFrame(f, this.waveFormColor));
+    if (this.hasShadow) {
+      this._drawMedianLine();
+      this._drawShadow();
+    }
   }
 
   _splitBy(size, list) {
@@ -67,6 +96,10 @@ export default class WaveForm extends AudioControl {
     return this._audioData;
   }
 
+  get audioLength() {
+    return this._audioData ? this._audioData.length : 0;
+  }
+
   get backgroundColor() {
     return this._options.backgroundColor || "#111";
   }
@@ -75,16 +108,28 @@ export default class WaveForm extends AudioControl {
     return this._audioData ? this._audioData.numberOfChannels : 0;
   }
 
+  get hasShadow() {
+    return this._options.hasShadow || false;
+  }
+
   get height() {
     return this._options.height || 80;
   }
 
-  get audioLength() {
-    return this._audioData ? this._audioData.length : 0;
+  get median() {
+    return this.height / 2;
   }
 
   get sampleRate() {
     return this._audioData ? this._audioData.sampleRate : 0;
+  }
+
+  get shadowColor() {
+    return this._options.shadowColor || "#fff";
+  }
+
+  get shadowOpacity() {
+    return this._options.shadowOpacity || 0.25;
   }
 
   get size() {
@@ -93,10 +138,6 @@ export default class WaveForm extends AudioControl {
 
   get waveFormColor() {
     return this._options.waveFormColor || "#f70";
-  }
-
-  get waveFormData() {
-    return this.audioData ? this._getAverages(this.audioData) : [];
   }
 
   get width() {
@@ -114,6 +155,6 @@ export default class WaveForm extends AudioControl {
     }
 
     this._audioData = data;
-    this._render();
+    this._render(this._audioData);
   }
 }
